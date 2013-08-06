7156c656ff2c182950e1fb1fda67323cc292edbf
karen chan <cee.wing@gmail.com>
2013-08-06 10:25:48 +0200

    Add media element.js and play the first track of the playlist

--- js/jammr_playlist.js
+++ js/jammr_playlist.js
@@ -2,12 +2,45 @@
     $.widget('jammr.playlist', {
 
         options: {
+            startPage: 0,
+            listSize: 10,
+            currentTrack: 0,
+            autoPlay: true,
+            playlist: [],
+            url: '',
+            mediaType: 'audio/mp4'
         },
 
         _create: function() {
+            this.page = this.options.startPage;
+            this.listSize = this.options.listSize;
+            this.currentTrack = this.options.currentTrack;
+            this.playlist = this.options.playlist;
+            this.url = this.options.url;
+            var this_ = this;
+            this.player = new MediaElementPlayer(this.element[0], {
+                type: this.options.mediaType,
+                success: function(mediaElement, domObject) {
+                    var track = this_.getTrack();
+                    if (track) {
+                        mediaElement.setSrc(track);
+                        mediaElement.load();
+                        if (this_.options.autoPlay) {
+                            mediaElement.play();
+                        }
+                    }
+                }
+            });
         },
 
         nextTrack: function() {
+            this.currentTrack++;
+        },
+
+        getTrack: function() {
+            if (this.playlist.length > 0) {
+                return this.playlist[this.currentTrack]['mixUrl'];
+            }
         },
 
     });
--- js/jammr_playlist_tests.js
+++ js/jammr_playlist_tests.js
@@ -1,7 +1,52 @@
 (function($) {
 
+    QUnit.testDone(function(details) {
+        $('.svg').remove();
+    });
+
     test('initialization of media player', function() {
-        ok(false, 'to be implemented');
+        strictEqual($('audio').length, 0, 'there should be no audio elements');
+        $('<audio>').appendTo('body').playlist({
+            autoPlay: false
+        });
+        strictEqual($('audio').length, 1, 'there should be one audio element');
+    });
+
+    test('initialization of playlist', function() {
+        $('<audio>').appendTo('body').playlist({
+            playlist: [{
+                "startDate": "2013-07-16T05:28Z",
+                "duration": "00:30:15",
+                "owner": "stefanha",
+                "users": ["stefanha", "lennard", "andhai"],
+                "url": "http://jammr.net/recorded_jams/2",
+                "mixUrl": "http://objects.dreamhost.com/jammr/20130803_1802_5luj3iKxLy.m4a",
+                "tracksUrl": "http://test.jammr.net/secret/tracks.zip"
+            }]
+        });
+        strictEqual($('audio').length, 1, 'there should be one audio element');
+        strictEqual($('audio')[0].paused, false, 'the audio should be playing automatically');
+        strictEqual($('audio')[0].muted, false, 'the audio should not be muted');
+        strictEqual($('audio')[0].src, 'http://objects.dreamhost.com/jammr/20130803_1802_5luj3iKxLy.m4a', 'the src should be set to the first mixUrl of the playlist');
+    });
+
+    test('auto play set to false', function() {
+        $('<audio>').appendTo('body').playlist({
+            autoPlay: false,
+            playlist: [{
+                "startDate": "2013-07-16T05:28Z",
+                "duration": "00:30:15",
+                "owner": "stefanha",
+                "users": ["stefanha", "lennard", "andhai"],
+                "url": "http://jammr.net/recorded_jams/2",
+                "mixUrl": "http://objects.dreamhost.com/jammr/20130803_1802_5luj3iKxLy.m4a",
+                "tracksUrl": "http://test.jammr.net/secret/tracks.zip"
+            }]
+        });
+        strictEqual($('audio').length, 1, 'there should be one audio element');
+        strictEqual($('audio')[0].paused, true, 'the audio should not be playing automatically');
+        strictEqual($('audio')[0].muted, false, 'the audio should not be muted');
+        strictEqual($('audio')[0].src, 'http://objects.dreamhost.com/jammr/20130803_1802_5luj3iKxLy.m4a', 'the src should be set to the first mixUrl of the playlist');
     });
 
 }(jQuery));
